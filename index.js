7042cec9e86392dfab440dab6572bf02e9996353
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2024-01-19 16:18:22 -0600

    updated index.js code

--- index.js
+++ index.js
@@ -2,8 +2,27 @@
 
 
 function greet() {
-    let userResponse = prompt("What is your name?");
-    alert("Hello " + userResponse + "!");
+    var userName = prompt('What is your name?');
+    if (userName && userName.trim()) {
+        alert('Hello, ' + userName + '!')
+    } else {
+        alert('Hello, How are you?')
+    }
+    var userAge = prompt("How old are you?");
+        alert(parseInt(userAge, 10))
+    var userBirthYear = confirm ('Have you had a birthday in the last year?');
+    if (userBirthYear) {
+        const userBirthYear = (parseInt(userAge, 10) - new Date().getFullYear());
+        alert('Your Birth Year is ' + userBirthYear)   
+    } else {
+        const userBirthDate = (parseInt(userAge, 10) - new Date().getFullYear());
+        alert('Your Birth Year is ' + userBirthYear)
+    }
+
     
-         
-}
+    
+        
+        
+    
+    }
+          
